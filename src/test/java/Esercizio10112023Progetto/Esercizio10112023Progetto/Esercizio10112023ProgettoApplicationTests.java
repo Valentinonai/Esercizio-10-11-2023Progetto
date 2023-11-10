4715aebbb63817e7887e9709b25d38bd6ef65475
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-11-10 09:26:23 +0100

    Initial commit

---
+++ src/test/java/Esercizio10112023Progetto/Esercizio10112023Progetto/Esercizio10112023ProgettoApplicationTests.java
@@ -0,0 +1,13 @@
+package Esercizio10112023Progetto.Esercizio10112023Progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esercizio10112023ProgettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
